8b66c5e87148e219c5b85fa565501ede50fca42c
DevMHOne <110272440+DevMHOne@users.noreply.github.com>
2024-08-01 18:40:42 +0700

    Update axios.js

--- plugins/axios.js
+++ plugins/axios.js
@@ -2,7 +2,7 @@ import axios from "axios"
 
 export default defineNuxtPlugin((NuxtApp) => {
     // axios.defaults.baseURL = 'http://localhost:8000'
-    axios.defaults.baseURL = 'https://api.johntest.site'
+    axios.defaults.baseURL = 'https://api.biolink.eu.org'
     axios.defaults.withCredentials = true;
 
     return {
